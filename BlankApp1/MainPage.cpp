e62aa2d466bf53f6b916110bcdcee38b68d240a7
lilac <github@nijisanji.sakura.ne.jp>
2021-02-28 22:53:06 +0900

    プロジェクト ファイルを追加します。

---
+++ BlankApp1/MainPage.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::BlankApp1::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+}
+
+
+void winrt::BlankApp1::implementation::MainPage::button1_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e)
+{
+    //追加
+    textBox1().Text(L"Hello xaml textBox1");
+}
+
+
+void winrt::BlankApp1::implementation::MainPage::button2_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e)
+{
+    //追加
+    textBox2().Text(L"Hello xaml textBox2");
+}
